37e7aebf2ee056069bf132a1d5260faaec76ef68
harshtiggenera <harsh.kalmegh@tiggenera.com>
2023-05-12 18:02:40 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 
-import Header from "./components/header/Header";
+import Header from "./components/headerSection/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import Category from "./components/Category/Category";
@@ -10,20 +10,20 @@ import Newsletter from "./components/Footer/Newsletter/Newsletter";
 import AppContext from "./utils/context";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <AppContext>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/category/:id" element={<Category />} />
-                    <Route path="/product/:id" element={<SingleProduct />} />
-                </Routes>
-                <Newsletter />
-                <Footer />
-            </AppContext>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <AppContext>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:id" element={<Category />} />
+          <Route path="/product/:id" element={<SingleProduct />} />
+        </Routes>
+        <Newsletter />
+        <Footer />
+      </AppContext>
+    </BrowserRouter>
+  );
 }
 
 export default App;
